6c2ae8697c572073796377d284bc28d86bfadd9b
Melikşah Selvi <mselvi@uni-yaz.com>
2023-02-11 14:42:26 +0300

    -vaadin kaldırıldı.

--- src/main/java/com/uniyaz/springboot/presentation/mis/markerpage/BaseEnum.java
+++
@@ -1,6 +0,0 @@
-package com.uniyaz.springboot.presentation.mis.markerpage;
-
-public interface BaseEnum {
-
-    String value();
-}
--- src/main/java/com/uniyaz/springboot/presentation/mis/markerpage/IMelikPage.java
+++
@@ -1,8 +0,0 @@
-package com.uniyaz.springboot.presentation.mis.markerpage;
-
-import com.vaadin.ui.Component;
-
-public interface IMelikPage {
-
-    Component tumSayfaView();
-}
