158d042e40060b36f487aff6b0437651444e11f4
Melikşah Selvi <mselvi@uni-yaz.com>
2023-02-11 14:43:54 +0300

    -lombok eklendi.

--- src/main/java/com/uniyaz/springboot/core/dto/PhoneExampleDto.java
+++ src/main/java/com/uniyaz/springboot/core/dto/PhoneExampleDto.java
@@ -1,5 +1,11 @@
 package com.uniyaz.springboot.core.dto;
 
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PhoneExampleDto {
 
     private String status;
@@ -9,60 +15,4 @@ public class PhoneExampleDto {
     private String internatiaonal_number;
     private String local_number;
     private String e164;
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPhone_type() {
-        return phone_type;
-    }
-
-    public void setPhone_type(String phone_type) {
-        this.phone_type = phone_type;
-    }
-
-    public String getCountry_code() {
-        return country_code;
-    }
-
-    public void setCountry_code(String country_code) {
-        this.country_code = country_code;
-    }
-
-    public String getCountry_prefix() {
-        return country_prefix;
-    }
-
-    public void setCountry_prefix(String country_prefix) {
-        this.country_prefix = country_prefix;
-    }
-
-    public String getInternatiaonal_number() {
-        return internatiaonal_number;
-    }
-
-    public void setInternatiaonal_number(String internatiaonal_number) {
-        this.internatiaonal_number = internatiaonal_number;
-    }
-
-    public String getLocal_number() {
-        return local_number;
-    }
-
-    public void setLocal_number(String local_number) {
-        this.local_number = local_number;
-    }
-
-    public String getE164() {
-        return e164;
-    }
-
-    public void setE164(String e164) {
-        this.e164 = e164;
-    }
 }
